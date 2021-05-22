48a417c0f477ff1a89649e65f08bf681146fd561
Sam Terfa <sam.terfa@reemahealth.com>
2021-05-22 14:12:20 -0500

    Update Plumber.R

--- Plumber.R
+++ Plumber.R
@@ -4,7 +4,7 @@ if(Sys.getenv('PORT') == '') Sys.setenv(PORT = 8000)
 #' @apiTitle Testing Gargle on GCP
 #' @apiDescription Testing Gargle on GCP
  
-#* @get /testing
+#* @get /scopes
 #* @param scopes Comma-separated list of scopes to check with credentials_gce
 #* @serializer text
 function(scopes){
@@ -13,5 +13,15 @@ function(scopes){
   print(scopes)
   token <- gargle::credentials_gce(scopes = scopes)
   
+  return(is.null(token))
+}
+
+#* @get /email
+#* @param scopes Comma-separated list of scopes to check with credentials_gce
+#* @serializer text
+function(){
+  
+  token <- gargle::credentials_gce()
+  print(token)
   return(is.null(token))
 }
