79643218877208f33c4b3c664dbb46bd7de3f3c8
Sam Terfa <sam.terfa@reemahealth.com>
2021-05-22 15:03:43 -0500

    credentials_gce2

--- Plumber.R
+++ Plumber.R
@@ -16,6 +16,23 @@ function(scopes){
   return(is.null(token))
 }
 
+
+#* @get /scopes2
+#* @param scopes Comma-separated list of scopes to check with credentials_gce
+#* @serializer text
+function(scopes){
+  
+  source('credentials_gce2.R')
+  
+  assignInNamespace('credentials_gce', credentials_gce2, ns = 'gargle')
+  
+  scopes <- strsplit(scopes, ',')[[1]]
+  
+  token <- gargle::credentials_gce(scopes = scopes)
+  
+  return(is.null(token))
+}
+
 #* @get /email
 #* @param scopes Comma-separated list of scopes to check with credentials_gce
 #* @serializer text
@@ -24,15 +41,4 @@ function(){
   token <- gargle::credentials_gce()
   
   return(gargle::token_email(token))
-}
-
-#* @get /instancescopes
-#* @serializer text
-function(){
-  
-  token <- gargle::credentials_gce()
-  
-  scopes <- gargle:::get_instance_scopes(gargle::token_email(token))
-  
-  return(paste(scopes, collapse = ', '))
 }
---
+++ credentials_gce2.R
@@ -0,0 +1,44 @@
+credentials_gce2 <- function(scopes = "https://www.googleapis.com/auth/cloud-platform",
+                            service_account = "default", ...) {
+  
+  print('Using updated credentials_gce function.')
+  
+  gargle:::gargle_debug("trying {.fun credentials_gce}")
+  if (!gargle:::detect_gce() || is.null(scopes)) {
+    return(NULL)
+  }
+  instance_scopes <- gargle:::get_instance_scopes(service_account = service_account)
+  # We add a special case for the cloud-platform -> bigquery scope implication.
+  if ("https://www.googleapis.com/auth/cloud-platform" %in% instance_scopes) {
+    instance_scopes <- c(
+      "https://www.googleapis.com/auth/bigquery",
+      instance_scopes
+    )
+  }
+  if (!all(scopes %in% instance_scopes)) {
+###########    return(NULL)
+  }
+  
+  gce_token <- gargle:::fetch_access_token(scopes, service_account = service_account)
+  
+  params <- list(
+    as_header = TRUE,
+    scope = scopes,
+    service_account = service_account
+  )
+  token <- GceToken$new(
+    credentials = gargle:::gce_token$access_token,
+    params = params,
+    # The underlying Token2 class appears to *require* an endpoint and an app,
+    # though it doesn't use them for anything in this case.
+    endpoint = httr::oauth_endpoints("google"),
+    app = httr::oauth_app("google", key = "KEY", secret = "SECRET")
+  )
+  token$refresh()
+  if (is.null(token$credentials$access_token) ||
+      !nzchar(token$credentials$access_token)) {
+    NULL
+  } else {
+    token
+  }
+}
